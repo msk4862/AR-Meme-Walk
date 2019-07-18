3fe2dd135e7dad38af39350e81295563265b5cb9
Shoaib <msk4862@gmail.com>
2019-07-19 01:00:24 +0530

    scripts added

---
+++ Assets/Scripts/PictureFactory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureFactory : MonoBehaviour {
+
+	public GameObject picPrefab;
+	public GoogleSearch google;
+
+	public void deleteOldPictures ()
+	{
+		if (transform.childCount > 0) {
+			foreach (Transform child in this.transform) {
+				Destroy(child.gameObject);
+			}
+		}
+	}
+
+	public void CreateImages (List<string> urlList, int resultNum, Vector3 camForward)
+	{
+		int picNum = 1;
+		Vector3 centre = Camera.main.transform.position;
+		foreach (string url in urlList) {
+			Vector3 pos = GetPosition(picNum, resultNum, camForward);
+			GameObject pic = Instantiate(picPrefab, pos, Quaternion.identity, this.transform);
+
+			pic.GetComponent<pictureBehaviour>().loadImage(url);
+			picNum++;
+		}
+	}
+
+	public Vector3 GetPosition (int picNum, int rowNum, Vector3 camForward)
+	{
+		Vector3 pos = Vector3.zero;
+
+		if (picNum <= 5) {
+			pos = camForward + new Vector3 (picNum * -3, 0, rowNum * 3.5f);
+		} else {
+			pos = camForward + new Vector3((picNum%5)* 3, 0, rowNum*3.5f);
+		}
+		return pos;
+	}
+}
---
+++ Assets/Scripts/pictureBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pictureBehaviour : MonoBehaviour {
+
+	public Renderer quadRenderer;
+	private Vector3 desiredPostion;
+	
+	// Use this for initialization
+	void Start () {
+		transform.LookAt(Camera.main.transform);
+		Vector3 desiredAngle = new Vector3(0, transform.localEulerAngles.y, 0);
+		transform.rotation = Quaternion.Euler( desiredAngle);
+
+		desiredPostion = transform.localPosition;
+		transform.localPosition = new Vector3(0, 20, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = Vector3.Lerp(transform.localPosition, desiredPostion, Time.deltaTime*4f);
+
+
+	}
+
+	public void loadImage (string url)
+	{
+		StartCoroutine(ImageFromUrl(url));
+	}
+
+	IEnumerator ImageFromUrl (string url)
+	{
+		WWW www = new WWW(url);
+		yield return www;
+
+		quadRenderer.material.mainTexture = www.texture;
+	}
+}
+
